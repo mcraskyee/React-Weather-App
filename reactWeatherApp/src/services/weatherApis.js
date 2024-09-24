5953dba8ca97c214f45f9e714b8ba211e03cb74d
Richard Yee <richardyee@192-168-1-6.tpgi.com.au>
2024-09-24 10:35:55 +1000

    first upload

---
+++ reactWeatherApp/src/services/weatherApis.js
@@ -0,0 +1,23 @@
+async function fetchWeather(searchCity, days, setData, setDays) {
+  const API_KEY = "f98472fece5e409d92293927242209";
+  const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${searchCity}&days=${days}&aqi=yes&alerts=yes`;
+
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error("failed to fetch city weather");
+    }
+    const result = await response.json();
+    if (result.location.name.toLowerCase() !== searchCity.toLowerCase()) {
+      setData("");
+      setDays(1);
+    } else {
+      setData(result);
+      setDays(days);
+    }
+  } catch (error) {
+    console.error("error fetching data", error);
+  }
+}
+
+export default fetchWeather;
